f85112156cbed675494ec1787ed9a19a87efa2d4
xjorgexqcx <jquijanoceli@gmail.com>
2014-01-13 17:43:44 -0500

    arregle el pom

---
+++ PCA01/src/test/java/com/spring/pca01/Sumar.java
@@ -0,0 +1,33 @@
+package com.spring.pca01;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class Sumar {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	int a, b;
+
+	@Before
+	public void setUp() throws Exception {
+		a = 6;
+		b = 8;
+	}
+
+	@Test
+	public void test() {
+		logger.info("" + (a + b));
+	}
+
+}
